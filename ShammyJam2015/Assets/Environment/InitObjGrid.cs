b34970b2e5220acc72b39d17e0b77d457abc4b72
Christopher Bridenstine <christopher.bridenstine@gmail.com>
2015-03-21 01:13:53 -0400

    CREATED A BASIC INITIALIZER FOR THE GRID
    * Created a basic initialize script for the grid called InitObjGrid
    
    * Created a basic object to anchor grid called ObjGrid
    
    * Created a basic sprite called square to represent a member of the grid
    
    * Created a test scene that contains an instance of ObjGrid
    
    * Created extraneous folders.

---
+++ ShammyJam2015/Assets/Environment/InitObjGrid.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitObjGrid : MonoBehaviour {
+
+	public GameObject[,] ObjGrid = new GameObject[10,10];
+	public int GridWidth;
+	public int GridHeight;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < GridHeight; i++){
+			for(int j = 0; j < GridWidth; j++){
+				// GameObject objType = Resources.Load("square") as GameObject;
+				GameObject obj = Instantiate(Resources.Load("square") as GameObject);
+				obj.transform.localPosition = new Vector3((1.0f * j) + 0.5f, (1.0f * i) + 0.5f, 0.0f);
+				obj.transform.localScale = new Vector3(2f, 2f, 2f);
+				ObjGrid[i,j] = obj;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
